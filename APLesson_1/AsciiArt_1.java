46b447716726363b06d36fb2557ad982556b4ce9
JacobGiedd <jacobgiedd@gmail.com>
2016-09-06 11:05:37 -0700

    Added files

---
+++ APLesson_1/AsciiArt_1.java
@@ -0,0 +1,17 @@
+public class firstProgram
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Soup es verry cew");
+		System.out.println("like verry gud 10/10");
+		
+		System.out.println("/$$$$$$ \\  /$$$$$$  /$$   /$$ /$$$$$$$  ");
+		System.out.println("/$$__  $$ /$$__  $$| $$  | $$| $$__  $$");
+		System.out.println("| $$  \\__/| $$  \\ $$| $$  | $$| $$  \\ $$");
+		System.out.println("|  $$$$$$ | $$  | $$| $$  | $$| $$$$$$$/");
+		System.out.println(" \\____  $$| $$  | $$| $$  | $$| $$____/ ");
+		System.out.println("/$$  \\ $$| $$  | $$| $$  | $$| $$ |    ");
+		System.out.println("|  $$$$$$/|  $$$$$$/|  $$$$$$/| $$ |     ");
+		System.out.println(" \\______/  \\______/  \\______/ |__/      ");
+	}
+}
---
+++ APLesson_1/AsciiArt_2.java
@@ -0,0 +1,22 @@
+public class ASCIIArt2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("                   YAao,");
+		System.out.println("                    Y8888b,");
+		System.out.println("                  ,oA8888888b,");
+		System.out.println("            ,aaad8888888888888888bo,");
+		System.out.println("         ,d888888888888888888888888888b,");
+		System.out.println("       ,888888888888888888888888888888888b,");
+		System.out.println("      d8888888888888888888888888888888888888,");
+		System.out.println("     d888888888888888888888888888888888888888b");
+		System.out.println("    d888888P'                    `Y888888888888,");
+		System.out.println("    88888P'                    Ybaaaa8888888888l");
+		System.out.println("   a8888'                      `Y8888P' `V888888");
+		System.out.println(" d8888888a                                `Y8888");
+		System.out.println("AY/'' `\\Y8b                                 ``Y8b");
+		System.out.println("Y'      `YP                                    ~~");
+		System.out.println("         `'");
+		
+	}
+}
