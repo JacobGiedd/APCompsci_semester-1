bc0330c21892b300c4e5fa4624c9197fda7a21a2
JacobGiedd <jacobgiedd@gmail.com>
2016-09-12 10:37:04 -0700

    Lab_3

---
+++ APLesson_3/Lab_03.java
@@ -0,0 +1,16 @@
+import java.util.scanner; //import Statement
+
+public class Lab_03
+{
+	public static void main(String[]args)
+	{
+		//instantiate the new scanner object "keyboard"
+		Scanner keyboard = new Scanner(System.in);
+		
+		//prompt for user input
+		System.out.println("How old are you? ");
+		
+		//search forthe next
+		
+	}
+}
