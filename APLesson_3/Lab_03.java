e515039d42dbde00924186471a6e87611b5852c3
JacobGiedd <jacobgiedd@gmail.com>
2016-09-12 12:16:03 -0700

    Lab 3 updates

--- APLesson_3/Lab_03.java
+++ APLesson_3/Lab_03.java
@@ -1,16 +1,46 @@
-import java.util.scanner; 
+import java.util.Scanner; 
 
 public class Lab_03
 {
 	public static void main(String[]args)
 	{
+		System.out.println("ayyy lmao! is I RudeAI");
+		
 		Scanner keyboard = new Scanner(System.in);
 		
-		System.out.println("How old M8?? ");
+		System.out.println("boi...gimmi ur name...");
+		String name = keyboard.next();
 		
-		int num = keyboard.nexxtInt();
+		System.out.println( name + "!??" + " who names a kid that...");
 		
-		System.out.println("Yesh! " + num + " is pretty  M80...");
+		System.out.println("Oi! How old are you M8?? ");
+		int num = keyboard.nextInt();
 		
+		System.out.println("Yesh! " + num + " is the worst year to be in M80");
+		
+		System.out.println("What do you do for fun?");
+		String fun = keyboard.next();
+		
+		System.out.println("I think only dorks enjoy " + fun + "... for real tho");
+		
+		System.out.println("what music do you like?");
+		String music = keyboard.next();
+		
+		System.out.println( music + "?" + " so your one of thoes people...");
+		
+		System.out.println("how manny siblings you got?");
+		int family = keyboard.nextInt();
+		
+		System.out.println( family + " ?!?!" + "that must suck");
+		
+		System.out.println("what do you want to be when your older");
+		String job = keyboard.next();
+		
+		System.out.println( job + "? " + "thats funny! " + "you need actually skill for that.");
+		
+		system.out.println
+		
+		
+
 	}
 }
