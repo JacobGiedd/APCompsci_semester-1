c55b0d68a71a4561be011ed91b0c24df43a7f31c
JacobGiedd <jacobgiedd@gmail.com>
2016-09-20 10:29:16 -0700

    Lab2

---
+++ APLesson_2/Lab_02.java
@@ -0,0 +1,30 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Objective_One");
+		int num1 = 9;
+		int num2 = 8;
+		
+		System.out.println("num1 multipied by num2 is " +" " + num1 * num2);
+		
+		System.out.println("Objective_Two");
+		
+		String first = "BillSmith";
+		String second = "wontonStreet";
+		String third = "SoupCity";
+		int forth = 1122;
+		System.out.println( first + second + third + forth);
+		
+		System.out.println("Objective_Three");
+		
+		int l = 8;
+		
+		int w = 5;
+		int h = 4;
+		int t = 2;
+		int recArea =  (2 * w * l ) + (2 * l * h) + (2 * h * w * l) ;
+		
+		System.out.println("the surface area of the rectangular cube is" +" "+ recArea);
+	}
+}
