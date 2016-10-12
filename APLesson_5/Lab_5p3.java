b20b9cec454724805e2c6f8220668be905e3efb5
JacobGiedd <GieddJ8897@sduhsd.lan>
2016-10-12 12:14:40 -0700

    lab 5p3

---
+++ APLesson_5/Lab_5p3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner; 
+
+public class Lab_5
+{
+	public static void main(String[]args)
+	{	//function
+		system.out.println("*GPA Calculator*");
+		
+		//scanners 
+		Lab_5p3 form = new Lab_5p3();
+		Scanner keyboard = new Scanner(System.in);
+		
+		//user imputs
+		System.out.println("please enter your 1st grade");
+			String gone = keyboard.next();
+			
+		System.out.println("please enter your 2nd grade");
+			String gtwo = keyboard.next();
+			
+		System.out.println("please enter your 3rd grade");
+			String gthree = keyboard.next();
+			
+		System.out.println("please enter your 4th grade");
+			String gfour = keyboard.next();
+			
+		System.out.println("please enter your 5th grade");
+			String gfive = keyboard.next();
+			
+		System.out.println("please enter your 6th grade");
+			String gsix = keyboard.next();
+			
+		System.out.println("please enter your 7th grade");
+			String gseven = keyboard.next();
+		
+		
+	}
+	
+}
