50e99a8fd9df690edbed9e0c6b6bbe5635b6c911
JacobGiedd <GieddJ8897@sduhsd.lan>
2016-12-05 12:07:51 -0800

    more fix
    get fixed a bit more

--- APLesson_10/Lab_10p2.java
+++ APLesson_10/Lab_10p2.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
-public class Lab_10
+public class Lab_10p2
 {
 	Scanner kb;
 	public static void main(String[]agrs)
 	{
 		Scanner kb = new Scanner(System.in);
-		System.out.println("*KEEP COMPOSITES*");
-		
+		System.out.println("*EXPRESSION SOLVER*");
 		
 	}
 }
