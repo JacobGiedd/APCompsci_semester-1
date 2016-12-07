fffdaf6ed2bd808936ede0a80ca8fdc336598a85
JacobGiedd <GieddJ8897@sduhsd.lan>
2016-12-07 10:43:53 -0800

    h
    f

---
+++ APLesson_11/Lab_11.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Lab_11
+{
+	Scanner kb;
+	public static void main(String[]agrs)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("*RANDOM NUMBERS*");
+		
+		
+	}
+}
