4f0858cef825948c6378531a4ba607c08b063e76
JacobGiedd <GieddJ8897@sduhsd.lan>
2017-01-20 12:08:59 -0800

    more

--- APLesson_12/Lab_12.java
+++ APLesson_12/Lab_12.java
@@ -7,7 +7,17 @@ public class Lab_12
 	Scanner kb;
 	public static void main(String[]agrs)
 	{
+		
 		Scanner kb = new Scanner(System.in);
 		System.out.println("*SPEED CALCULATOR*");
+		
+		//USER IMPUTS/ INSTANCE VARIABLES
+		System.out.println("-IMPUT DISTANCE-");
+			double d = keyboard.nextInt();
+		System.out.println("-IMPUT HOURS-");
+			int h = keyboard.nextInt();
+		System.out.println("-IMPUT MINUTES-");
+			int m = keyboard.nextInt();
+			
 	}
 }
